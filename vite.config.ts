59e36a4dd72a5227307644bfc636d565a6cc78ed
vrezhMik <mikayelyan.vrezh99@gmail.com>
2025-02-18 23:34:42 +0400

    add: pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/dooin_landing/', // Ensure this exactly matches your GitHub repo name!
+  base: '/dooin_landing_pages/', // Ensure this exactly matches your GitHub repo name!
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 })
